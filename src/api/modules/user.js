10cd48497160d7546df48f06a90629e60186e71a
Fukusan64 <ikishikawa64@gmail.com>
2019-01-13 05:27:18 +0900

    実際にapiを叩くようにした

--- src/api/modules/user.js
+++ src/api/modules/user.js
@@ -1,11 +1,10 @@
-//------ for debug --------
-const sleep =  (msec) => new Promise(
-  (resolve) => setTimeout(resolve, msec)
-);
-//-------------------------
+import axios from "axios";
+
+const BASE_URL = process.env.VUE_APP_ATCODER_PROBLEM_RECOMMENDER_API_BASE_URL || '.';
+const crossdomain = process.env.VUE_APP_API_IS_CROSSDOMAIN === 'CROSSDOMAIN';
 const getList = async () => {
-	await sleep(2000);
-	return ['unit', 'fukusan', 'masu'];
+	const { data } = (await axios.get(`${BASE_URL}/Userlist.txt`, { crossdomain }));
+	return data.split('\n');
 }
 
 export default {
