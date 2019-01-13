25be28fef0a15971bb48eb9f051b1daed52455af
Fukusan64 <ikishikawa64@gmail.com>
2019-01-13 15:08:54 +0900

    問題取得apiを実装

--- src/api/modules/recommendProblem.js
+++ src/api/modules/recommendProblem.js
@@ -1,13 +1,21 @@
-//------ for debug --------
-const sleep =  (msec) => new Promise(
-  (resolve) => setTimeout(resolve, msec)
-);
-//-------------------------
+import axios from "axios";
+
+const BASE_URL = process.env.VUE_APP_ATCODER_PROBLEM_RECOMMENDER_API_BASE_URL || '.';
+const crossdomain = process.env.VUE_APP_API_IS_CROSSDOMAIN === 'CROSSDOMAIN';
+
+
 const getList = async (userName) => {
-  await sleep(2000);
   // eslint-disable-next-line no-console
-  console.log(userName);
-  return [];
+  const { data } = (await axios.get(`${BASE_URL}/Recommend_for_${userName}.txt`, { crossdomain }));
+  return data.split('\n').map(e => {
+    const [problem, evaluationValue] = e.split(' ');
+    const contestName = problem.replace(/_[^_]*$/,'').replace('_','-');
+    return {
+      problem,
+      evaluationValue,
+      link: `https://atcoder.jp/contests/${contestName}/tasks/${problem}`
+    }
+  });
 }
 
 export default {
